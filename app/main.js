7759eec806e8ae86a1544f8a2962b3d39d2e1e19
Thamizhiniyan <thamizhiniyan@localhost.localdomain>
2024-03-29 22:46:46 +0530

    stage 7

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("node:fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -24,11 +25,24 @@ const server = net.createServer((socket) => {
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`
       );
     } else if (path.startsWith("/user-agent")) {
-      const userAgent = request[2].split(" ")[1]
+      const userAgent = request[2].split(" ")[1];
 
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       );
+    } else if (path.startsWith("/files")) {
+      directory = process.argv[3];
+      filename = path.split("/files/")[1];
+
+      fs.readFile(directory + filename, "utf8", (err, data) => {
+        if (err) {
+          socket.write("HTTP/1.1 404 OK\r\n\r\n");
+        }
+
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`
+        );
+      });
     } else {
       socket.write("HTTP/1.1 404 OK\r\n\r\n");
     }
